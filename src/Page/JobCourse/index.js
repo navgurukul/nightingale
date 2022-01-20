e841fbba32ff519920ba06fcaf5fded8c0d0c83c
Vinay Lakhani <66902288+bunnie26@users.noreply.github.com>
2022-01-21 01:09:34 +0530

    completed job course page

--- src/Page/JobCourse/index.js
+++ src/Page/JobCourse/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
-
+import { Link } from "react-router-dom";
 function JobCourse() {
   return (
     <main className="job-course-page">
-      <h2 className="job-course-title">Gauranteed Job Courses</h2>
+      <h2 className="job-course-title section-head fs-3">
+        Gauranteed Job Courses
+      </h2>
       <div className="job-course-content w-100 d-flex justify-content-center">
-        <p className="w-40">
+        <p className="w-40 section-para">
           Fully-funded one-year software engineering program to youth from
           low-income families.
         </p>
@@ -15,30 +17,38 @@ function JobCourse() {
       <div className="container ">
         <div className="row">
           <div className="col my-3 col-md-6">
-            <div className="card programmer-img-card">
-              <div class="card-body p-0 pt-3">
-                <img
-                  src={require("./assets/programmers.png").default}
-                  className="job-course-img"
-                />
-                <p className="mt-3 mb-3 fs-1.1">Programmers’ Gurukul</p>
+            <Link to="/programmer ">
+              <div className="card programmer-img-card">
+                <div class="card-body p-0 pt-3">
+                  <img
+                    src={require("./assets/programmers.png").default}
+                    className="job-course-img"
+                  />
+                  <p className="mt-3 mb-3 px-2 fs-1.1 section-para">
+                    Programmers’ Gurukul
+                  </p>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
           <div className="col my-3 col-md-6">
-            <div className="card design-img-card">
-              <div class="card-body p-0 pt-3">
-                <img
-                  src={require("./assets/designers.png").default}
-                  className="job-course-img"
-                />
-                <p className="mt-3 mb-3 fs-1.1" >Designers’ Gurukul</p>
+            <Link to="#">
+              <div className="card design-img-card">
+                <div class="card-body p-0 pt-3">
+                  <img
+                    src={require("./assets/designers.png").default}
+                    className="job-course-img"
+                  />
+                  <p className="mt-3 px-2 mb-3 fs-1.1 section-para">
+                    Designers’ Gurukul
+                  </p>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
-      <div className="coming-soon">
+      <div className="coming-soon section-para">
         <p>Coming soon: Justice Gurukul</p>
       </div>
     </main>
