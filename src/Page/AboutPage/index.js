664c8f56abcabbfd75b0601412d07d0d36fc76d1
Vinay Lakhani <lakhani.vinay.6377@gmailcom>
2022-01-20 13:44:05 +0530

    about page completed

--- src/Page/AboutPage/index.js
+++ src/Page/AboutPage/index.js
@@ -1,36 +1,25 @@
 import React from "react";
 import "./styles.css";
+import backgroundImg from "./assets/section-bg.png";
 
 function AboutPage() {
   return (
     <main className="about-page">
       <div className="page-content">
         <section className="page-section">
-          <h2 className="section-title">Our Story</h2>
-          <hr className="mt-2" />
+          <h2 className="section-head">Our Story</h2>
+          <hr className="heading-hr mt-2" />
           <div>
-            <img
-              src={require("./assets/Ellipse_1.png").default}
-              className="Ellipse_1"
-            />
-            <img
-              src={require("./assets/Ellipse_2.png").default}
-              className="Ellipse_2"
-            />
-            <img
-              src={require("./assets/Ellipse_3.png").default}
-              className="Ellipse_3"
-            />
-            <img src={require("./assets/bg.png").default} className="bg-img" />
+            <img src={backgroundImg} className="backgroundImg" />
           </div>
           <div className="about-page-content d-flex flex-column justify-content-center align-items-center">
-            <p className="w-50">
+            <p className="w-50 section-para">
               Started in 2016, Navgurukul runs one-year residential course for
               students from underserved and diverse communities. Students after
               pursuing our course are awarded Certificate in Software
               Programming.
             </p>
-            <p className="w-50 mt-3">
+            <p className="w-50 mt-3 section-para">
               In last five years, more than 160 students from marginalised areas
               have got jobs as Software Programs and have donated back more than
               28 Lacs in donation. We have diversified to add School of Design
@@ -40,10 +29,10 @@ function AboutPage() {
           </div>
         </section>
         <section className="page-section">
-          <h2 className="section-title">Vision</h2>
-          <hr className="mt-2" />
+          <h2 className="section-head">Vision</h2>
+          <hr className="heading-hr mt-2" />
           <div className="about-content d-flex flex-column justify-content-center align-items-center">
-            <p className="w-50">
+            <p className="w-50 section-para">
               We envision a world where everyone has access to an affordable
               world class education that enables them to live up to their
               highest potential
@@ -51,15 +40,15 @@ function AboutPage() {
           </div>
         </section>
         <section className="page-section">
-          <h2 className="section-title">Our Values</h2>
-          <hr className="mt-2" />
+          <h2 className="section-head">Our Values</h2>
+          <hr className="heading-hr mt-2" />
           <div className="about-content d-flex flex-column justify-content-center align-items-center">
-            <p className="w-50">
+            <p className="w-50 section-para">
               In order to follow through on our commitments, achieve our
               mission, and realise our vision, we’ve created a set of values
               made easy to remember through our acronym VELOCITY.
             </p>
-            <p className="w-50 mt-3">
+            <p className="w-50 mt-3 section-para">
               Let’s break it down: V - Velocity, E - Entrepreneurial Mindset, L
               - Love for customers, O - Out of the box thinking, C -
               Collaboration, I - Impact, T - Trust, Y - You.
