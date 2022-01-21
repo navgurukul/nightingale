2755a253ffec87fad66f4dbec9455d55525ed918
Vinay Lakhani <66902288+bunnie26@users.noreply.github.com>
2022-01-22 00:33:43 +0530

    hiring page done

--- src/Page/NgHiring/index.js
+++ src/Page/NgHiring/index.js
@@ -1,40 +1,33 @@
 import React from "react";
 import "./styles.css";
+import backgroundImg from "./assets/section-bg.png";
 
 function NgHiring() {
   return (
     <main className="ng-hiring-page">
       <div className="page-content">
-        <section className="hiring-page-section">
-          <h2 className="hiring-section-title">Hire from Us</h2>
-          <hr className="my-3" />
-          <div className="mb-5">
-            <img
-              src={require("./assets/Ellipse_1.png").default}
-              className="Ellipse_1"
-            />
-            <img
-              src={require("./assets/Ellipse_2.png").default}
-              className="Ellipse_2"
-            />
-            <img
-              src={require("./assets/Ellipse_3.png").default}
-              className="Ellipse_3"
-            />
-            <img src={require("./assets/bg.png").default} className="bg-img" />
+        <section className="hiring-page-section mb-5">
+          <h2 className="section-head mt-5">Hire from Us</h2>
+          <hr className="heading-hr" />
+          <div className="mt-5">
+            <img src={backgroundImg} className="backgroundImg" />
           </div>
-          <div className="mt-5 hiring-page-content">
-            <p>
-              We are a collective of full timers and volunteers that form the
-              backbone aiming to bring affordable education to underprivileged
-              girl students across India.
+          <div className="mt-4 mb-2 d-flex w-100 justify-content-center hiring-page-content">
+            <p className="section-para w-50">
+              Our Software Engineering and Graphic Design graduates are skilled
+              to be productive from Day 1. Take a look at these{" "}
+              <b className="fw-bold"> diamond in the roughs</b> who are polished
+              gems now.
             </p>
           </div>
-          <button type="button" class="hiring-page-button">
+          <button
+            type="button"
+            class="btn mb-4 f-Nuni fw-semiBold py-2 ng-labs-btn"
+          >
             Download Placement Brief
           </button>
           <div className="hiring-page-content">
-            <p>
+            <p className="section-para mb-5">
               Contact us directly at{" "}
               <a href="#" className="link">
                 tnp@navgurukul.org
@@ -43,9 +36,11 @@ function NgHiring() {
             </p>
           </div>
         </section>
-        <section className="hiring-page-section">
-          <h2 className="hiring-section-title">Meet Our Recent Graduates</h2>
-          <hr />
+        <section className="hiring-page-section my-5">
+          <h2 className="hiring-section-title section-head mt-3">
+            Meet Our Recent Graduates
+          </h2>
+          <hr className="heading-hr" />
         </section>
       </div>
     </main>
