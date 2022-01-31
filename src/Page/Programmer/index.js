1a6a283e5b9c02e785138db8fd5bbd4e1b6cdb0f
Vinay Lakhani <lakhani.vinay.6377@gmailcom>
2022-01-31 19:02:37 +0530

    designer's page and programmer's page done

--- src/Page/Programmer/index.js
+++ src/Page/Programmer/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./style.css";
 import "../../Global.css";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import icon1 from "./assets/icon1.png";
 import icon2 from "./assets/icon2.png";
 import video from "./assets/video.mp4";
@@ -17,9 +18,11 @@ function Programmer() {
           <h2 className="fs-1.5 section-head text-black fw-bold mb-0">
             Programmers’ Gurukul{" "}
           </h2>
-          <span className="desktop d-block f-Nuni  fs-1.2 text-primary fw-bold">
-            Change Gurukul
-          </span>
+          <Link to="/job-course">
+            <span className="desktop d-block f-Nuni text-primary fw-bold">
+              Change Gurukul
+            </span>
+          </Link>
         </div>
       </div>
       <div className="hero-P container my-2 my-sm-5">
@@ -53,14 +56,17 @@ function Programmer() {
                 To be a part of the Gurukul, all it takes is some determination
                 and potential.
               </p>
+
               <button
                 className="btn btn-primary text-white w-100 my-1 fs-sm-1.2  ng-labs-btn"
-                onClick={() => {
-                  setState("");
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open("https://admissions.navgurukul.org/", "_blank");
                 }}
               >
                 Take Test
               </button>
+
               {/* <div className="text fs-sm-1.2 fs-1.1 my-1">
               Had a test already?{" "}
               <span className="text-primary">Check status here</span>
@@ -121,7 +127,13 @@ function Programmer() {
         <div className="container w-100 my-4 d-flex align-items-center justify-content-center">
           <div className="row d-flex flex-md-row flex-column justify-content-center w-100">
             <div className="col-12 col-md-6 my-2 d-flex justify-content-center">
-              <div className="card p-3 d-flex flex-column mh-10 bg-bg-card-1 justify-content-top align-items-center text-left text-sm-center w-100 rounded border-0 ">
+              <div
+                className="card p-3 d-flex flex-column mh-10 bg-bg-card-1 justify-content-top align-items-center text-left text-sm-center w-100 rounded border-0 "
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open("https://admissions.navgurukul.org/", "_blank");
+                }}
+              >
                 <h2 className="fw-bold my-1 section-head text-center">
                   Online Tests
                 </h2>
@@ -153,18 +165,18 @@ function Programmer() {
         <div className="container w-100 mt-3  d-flex justify-content-center">
           <div className="row d-flex flex-md-row flex-column justify-content-center w-100 student-video-parent">
             <div className="col-12 col-md-4 my-3 d-flex justify-content-center ">
-              <video src={video} width="330px" controls></video>
+              <video src={video} width="360" height="200" controls></video>
             </div>
             <div className="col-12 col-md-4 my-3 d-flex justify-content-center">
-              <video src={video} width="330px" controls></video>
+              <video src={video} width="360" height="200" controls></video>
             </div>
             <div className="col-12 col-md-4 my-3 d-flex justify-content-center">
-              <video src={video} width="330px" controls></video>
+              <video src={video} width="360" height="200" controls></video>
             </div>
           </div>
         </div>
       </div>
-      <div className="next-step my-2 my-sm-5 d-flex flex-column align-items-center">
+      <div className="next-step mt-2 mt-sm-5 d-flex flex-column align-items-center">
         <h2 className="fw-semiBold mb-3 section-head">
           Ready for the Next Step?
         </h2>
@@ -182,10 +194,12 @@ function Programmer() {
             setState(e.target.value);
           }}
         /> */}
+          {/* <a></a> */}
           <button
             className="btn btn-primary f-Nuni fw-semiBold text-white w-100 my-1 ng-labs-btn"
-            onClick={() => {
-              setState("");
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("https://admissions.navgurukul.org/", "_blank");
             }}
           >
             Take Test
