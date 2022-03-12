8ed4734f3ce4a3643fe7e84bb9e149833a300ed4
Vinay Lakhani <66902288+bunnie26@users.noreply.github.com>
2022-03-12 10:37:49 +0530

    removed commented code

--- src/Page/Header/index.js
+++ src/Page/Header/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import JobCourse from "../JobCourse";
 import { FiExternalLink } from "react-icons/fi";
 import "./styles.css";
 import externalLink from "./assets/external-link.png";
