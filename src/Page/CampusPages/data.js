8f587cd51e27423d206f058449003dbdf001d9cf
Vinay Lakhani <66902288+bunnie26@users.noreply.github.com>
2022-03-25 14:03:44 +0530

    created structure of campus page

---
+++ src/Page/CampusPages/data.js
@@ -0,0 +1,43 @@
+const Campus_data = {
+  Pune: {
+    name: "Pune Campus",
+    manager: "Neelam Khetri (Campus Manager)",
+    address: "Khed-Shivapur Toll Plaza, Khopi, Maharashtra 412205",
+    capacity: "200",
+    faculty: "50",
+    contact: "123456789",
+    location:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3787.3479732825235!2d73.85025791488987!3d18.33140278751951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2ed3065c3fa03%3A0x3fdbebfaf1c4b19d!2sKhed-Shivapur%20Toll%20Plaza%2C%20Maharashtra%20412205!5e0!3m2!1sen!2sin!4v1648096644567!5m2!1sen!2sin",
+  },
+  Bengaluru: {
+    name: "Banglore Campus",
+    manager: "ABC...XYZ",
+    address: "Khed-Shivapur Toll Plaza, Khopi, Maharashtra 412205",
+    capacity: "400",
+    faculty: "5",
+    contact: "588456789",
+    location:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.6148868269765!2d77.7108817148209!3d12.86813239092383!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6d4d12f750d7%3A0x74f3ba658fbf7d95!2sNavGurukul%20Bangalore%20Campus!5e0!3m2!1sen!2sin!4v1648140375679!5m2!1sen!2sin",
+  },
+  Dharamshala: {
+    name: "Dharamshala Campus",
+    manager: "Vinay",
+    address: "Khed-Shivapur Toll Plaza, Khopi, Maharashtra 412205",
+    capacity: "700",
+    faculty: "15",
+    contact: "0578989",
+    location:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3376.8260602373425!2d76.342372315167!3d32.18196488115586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391b51969f04e4b9%3A0x26c06669e05c031b!2sNavGurukul%20Dharamsala%20Campus!5e0!3m2!1sen!2sin!4v1648141320246!5m2!1sen!2sin",
+  },
+  Sarjapur: {
+    name: "Sarjapur Campus",
+    manager: "Bunnie",
+    address: "cvgcvegvcefvv ccee dede",
+    capacity: "800",
+    faculty: "20",
+    contact: "123479",
+    location:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124462.80258686819!2d77.66731785784104!3d12.877949598684385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae137aa6184017%3A0xb8ee2b373af46534!2sNavGurukul%20Sarjapur%20Campus!5e0!3m2!1sen!2sin!4v1648141413838!5m2!1sen!2sin",
+  },
+};
+export default Campus_data;
--- src/Page/CampusPages/index.js
+++ src/Page/CampusPages/index.js
@@ -1,58 +1,142 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import campusImg1 from "./assets/campus_1.png";
+import campusImg2 from "./assets/campus_2.png";
+import avatar from "./assets/bg.png";
+import Campus_data from "./data";
 
 function CampusPages() {
   const [campus, setCampus] = useState("Pune");
+  const [data, setData] = useState(Campus_data[campus]);
+  useEffect(() => {
+    setData(Campus_data[campus]);
+  }, [campus]);
   return (
-    <div className="campus d-flex flex-column justify-content-center">
-      <div className="campus-description d-flex flex-column justify-content-center align-items-center">
+    <div className="campus-page d-flex flex-column justify-content-center">
+      <div className="campus-description mb-3 d-flex flex-column justify-content-center align-items-center">
         <h3 className="mb-3">Our Campuses</h3>
         <hr className="heading-hr mt-0" />
 
-        <div className="campuses d-flex col-md-6 justify-content-around">
-          <div>
-            <input
-              className="mr-1"
-              type="radio"
-              value="Pune"
-              onClick={(e) => {
-                console.log(e.target.value);
-              }}
-            />
-            <label>Pune</label>
+        <div className="campuses d-flex col-12 col-md-6 justify-content-around mb-3 pb-0 pb-md-3">
+          <div className="campus-btn mr-2">
+            <label className="d-flex align-items-center">
+              <input
+                className="mr-1 mr-md-2"
+                name="campuses"
+                type="radio"
+                value="Pune"
+                onClick={(e) => {
+                  setCampus(e.target.value);
+                  // setCampus("Pune");
+                }}
+                checked={campus === "Pune"}
+              />
+              Pune
+            </label>
           </div>
-          <div>
-            <input
-              className="mr-1"
-              type="radio"
-              value="Bengaluru"
-              onClick={(e) => {
-                console.log(e.target.value);
-              }}
-            />
-            <label>Bengaluru</label>
+          <div className="campus-btn mr-2">
+            <label className="d-flex align-items-center">
+              <input
+                className="mr-1 mr-md-2"
+                name="campuses"
+                type="radio"
+                value="Bengaluru"
+                onClick={(e) => {
+                  setCampus(e.target.value);
+                  // setCampus("Bengaluru");
+                }}
+                checked={campus === "Bengaluru"}
+              />
+              Bengaluru
+            </label>
           </div>
-          <div>
-            <input
-              className="mr-1"
-              type="radio"
-              value="Sarjapur"
-              onClick={(e) => {
-                console.log(e.target.value);
-              }}
-            />
-            <label>Sarjapur</label>
+          <div className="campus-btn mr-2">
+            <label className="d-flex align-items-center">
+              <input
+                className="mr-1 mr-md-2"
+                name="campuses"
+                type="radio"
+                value="Sarjapur"
+                onClick={(e) => {
+                  setCampus(e.target.value);
+                  // setCampus("Sarjapur");
+                }}
+                checked={campus === "Sarjapur"}
+              />
+              Sarjapur
+            </label>
           </div>
-          <div>
-            <input
-              className="mr-1"
-              type="radio"
-              value="Dharamshala"
-              onClick={(e) => {
-                console.log(e.target.value);
-              }}
+          <div className="campus-btn mr-2">
+            <label className="d-flex align-items-center">
+              <input
+                className="mr-1 mr-md-2"
+                name="campuses"
+                type="radio"
+                value="Dharamshala"
+                onClick={(e) => {
+                  setCampus(e.target.value);
+                  // setCampus("Dharamshala");
+                }}
+                checked={campus === "Dharamshala"}
+              />
+              Dharamshala
+            </label>
+          </div>
+        </div>
+        <div className="container d-flex-column d-md-flex">
+          <div className="col-12 col-md-5 d-flex flex-column">
+            <h4 className="mb-3 campus-name">{data.name}</h4>
+            <div className="mb-3 campus-manager">
+              <img src={avatar} className="mr-3" />
+              {data.manager}
+            </div>
+            <div className="mb-3 campus-address">
+              <span className="fw-bold">Address : </span>
+              {data.address}
+            </div>
+            <div className="mb-3 campus-capacity">
+              <span className="fw-bold">Student Capacity : </span>
+              {data.capacity}
+            </div>
+            <div className="mb-3 campus-faculty">
+              <span className="fw-bold">Faculty : </span>
+              {data.faculty}
+            </div>
+            <div className="mb-3 campus-contact">
+              <span className="fw-bold">Contact : </span>
+              {data.contact}
+            </div>
+          </div>
+          <div className="col-12 col-md-7">
+            <iframe
+              src={data.location}
+              // width="640"
+              // height="360"
+              style={{ border: 0, borderRadius: "8px" }}
+              // allowfullscreen="true"
+              className="campus_map"
+              loading="none"
             />
-            <label>Dharamshala</label>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <hr className="campus-dividerLine" />
+      </div>
+      <div className="campus-gallary mt-3 pb-3 mb-5">
+        <div className="container">
+          <div className="row pb-3">
+            <div className="col-12 py-3 py-md-0 col-md-6">
+              <img className="campus_shortImg" src={campusImg1} />
+            </div>
+            <div className="col-12 py-3 py-md-0 col-md-6">
+              <img className="campus_shortImg" src={campusImg1} />
+            </div>
+          </div>
+          <div className="row py-0 py-md-3">
+            <div className="col-12">
+              <img className="campus_longImg" src={campusImg2} />
+            </div>
           </div>
         </div>
       </div>
