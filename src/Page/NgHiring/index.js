9f1465cee76fd93ebfd44271953419794d027adb
Apekshahande <apeksha18@navgurukul.org>
2024-05-07 16:23:07 +1100

    made few changes in ng hiring page

--- src/Page/NgHiring/index.js
+++ src/Page/NgHiring/index.js
@@ -64,23 +64,49 @@ function NgHiring() {
   }, []);
   const [team, setTeam] = useState([]);
   return (
-    <main className="ng-hiring-page">
-      <div className="page-content">
-        <section className="container hiring-page-section d-flex flex-column w-100 mb-3">
-          <h3 className="section-head mt-3 mt-md-5 mb-3">Hire from Us</h3>
-         
-          <div className="mt-3">
-            <img src={backgroundImg} className="backgroundImg" />
-          </div>
-          <div className="mt-4 mb-2 d-flex w-100 justify-content-center hiring-page-content">
-            <p className="section-para w-70">
-              Our Software Engineering and Graphic Design graduates are skilled
-              to be productive from Day 1. Take a look at these{" "}
-              <b className="fw-bold"> diamond in the roughs</b> who are polished
-              gems now.
-            </p>
-          </div>
-          <a
+    <main >
+      <section className="container align-items-center d-flex flex-column  justify-content-center w-100">
+        <h3 className="section-head mt-3 mt-md-5 mb-3">Hire from Us</h3>
+        <div className=" hiring-page-content col-12 p-0 col-md-8 section-para d-flex align-items-center justify-content-center mb-2 mt-3">
+          <p className="section-para w-100" >
+            Through harnessing the power of education, We bridge the learning 
+            gap of students from marginalized communities to emerge as skilled,
+            resilient, and job-ready professionals, proficient in the latest technologies
+            and soft skills. By partnering with us, your organization can enrich its 
+            workforce diversity and drive meaningful social impact, effortlessly meeting
+            your Diversity, Equity, and Inclusion (DEI) goals.
+          </p>
+        </div>
+        <div className=" hiring-page-content col-12 p-0 col-md-8 section-para d-flex align-items-center justify-content-center mb-2 mt-3">
+          <p className="section-para w-100" >
+            By hiring from us, You are achieving : 
+            <ul className="ml-4">
+              <li>
+                Handpicked Tech Talent: Pre-screened candidates to save your time.
+              </li>
+              <li>
+                Zero Hiring Fees: Fully funded by our generous donors.
+              </li>
+              <li>
+                High Retention candidates: Quality talent within budget</li>
+              <li>
+                Diverse Talent Pool: Our students are from different regions 
+                of India. Ready to enhance your workforce diversity and business capabilities.
+              </li>
+              <li>
+                Job-Ready Talent: Our students undergo rigorous training in both 
+                technical and soft skills, preparing them for the complexities of modern
+                work environments.
+              </li>
+              <li>
+                Positive Social Impact: Fulfill your corporate social responsibility goals by hiring candidates who are part of a movement to bridge the education and employment gap in India.
+              </li>
+            </ul>
+          </p>
+          
+
+        </div>
+         <a
             href="https://drive.google.com/file/d/1sem3BGOxBscPY2KMOcc_KAZ1ZJSJ-IeE/view?usp=sharing"
             download="Placement Brief - Navgurukul"
             target="_blank"
@@ -92,8 +118,8 @@ function NgHiring() {
               Download Placement Brief
             </button>
           </a>
-          <div className="hiring-page-content d-flex justify-content-center ">
-            <p className="section-para">
+          <div className="hiring-page-content d-flex justify-content-center align-items-center col-md-5">
+            <p className="section-para text-center">
               Contact us directly at{" "}
               <a href="mailto:tnp@navgurukul.org" className="link">
                 tnp@navgurukul.org
@@ -101,10 +127,12 @@ function NgHiring() {
               for hiring or freelancing projects.
             </p>
           </div>
-        </section>
-        <section className="hiring-page-section mb-4 d-flex flex-column mt-5">
+         
+        </section>    
+    
+        <section className="ng-hiring-page hiring-page-section mb-4 d-flex flex-column mt-5">
           <h3 className="hiring-section-title section-head  mb-3">
-            Meet Our Recent Graduates
+            Meet Our Alumni
           </h3>
           <div className="container hiring-page-card-container px-0 d-flex mt-3">
             {Object.keys(shuffleObject(team)).length ? (
@@ -174,7 +202,7 @@ function NgHiring() {
             )}
           </div>
         </section>
-      </div>
+      {/* </div> */}
     </main>
   );
 }
