c71eb5635e5ef4c20fc0c67953a10203e93494b2
Alpana Chavan <alpanachavan20@navgurukul.org>
2024-05-21 11:10:16 +0530

    added social media links or added admission url

--- src/Page/SchoolsPages/CourseLocations/index.js
+++ src/Page/SchoolsPages/CourseLocations/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./style.css"
+import "./style.css";
 import { useHistory } from "react-router-dom";
 
 function CoursesLocation({ courses }) {
-
     const [data, setData] = useState(null);
     const history = useHistory();
+
     useEffect(() => {
         axios.get("https://navgurukul.github.io/tarabai-shinde/data/campuses.json")
             .then((res) => {
@@ -24,21 +24,22 @@ function CoursesLocation({ courses }) {
             </div>
         );
     }
+
     const programmingCampuses = Object.values(data).filter(campus => campus["Courses offered"] && campus["Courses offered"].includes(courses));
+
     return (
-        <div className="partners-data w-100 mb-0 md-3 d-flex align-items-center flex-column" style={{ marginTop: "80px"}}>
+        <div className="partners-data w-100 mb-0 md-3 d-flex align-items-center flex-column" style={{ marginTop: "80px" }}>
             <h3 className="text-center" style={{ marginBottom: "32px" }}>
                 <span className="BackColor">Programme</span> Locations
             </h3>
             {programmingCampuses.length === 1 ? (
                 <div className="single-partner w-75 d-flex flex-column align-items-center">
-                    <div className="col-sm-12 col-lg-7 col-md-12 align-items-center" style={{ marginBottom: "14px", marginRight: "20px"}}>
+                    <div className="col-sm-12 col-lg-7 col-md-12 align-items-center" style={{ marginBottom: "14px", marginRight: "20px" }}>
                         <div className="p-2">
                             <div className="campus-title-design">
                                 <h5 className="card-title campus-fontfamily" style={{ fontSize: "24px" }}>
                                     {programmingCampuses[0].Name}
                                 </h5>
-                               
                             </div>
                             <span className="">
                                 <span className="fw-bold">Address: </span>{programmingCampuses[0].Address}
@@ -49,11 +50,13 @@ function CoursesLocation({ courses }) {
                                 </span>
                             </div>
                             <div className="mt-3">
-                                <img
-                                    src={require("../assets/instagram.png").default}
-                                    alt=""
-                                    className="instagram"
-                                />
+                                <a href={programmingCampuses[0].Instagram} target="_blank" rel="noopener noreferrer">
+                                    <img
+                                        src={require("../assets/instagram.png").default}
+                                        alt="Instagram"
+                                        className="instagram"
+                                    />
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -64,11 +67,10 @@ function CoursesLocation({ courses }) {
                         <div className="container-fluid overflow-hidden">
                             <div className="row">
                                 {programmingCampuses.map((campus) => (
-                                    <div className="col-sm-12 col-lg-6 col-md-12 align-items-center" style={{ marginBottom: "32px" }}>
+                                    <div key={campus.Name} className="col-sm-12 col-lg-6 col-md-12 align-items-center" style={{ marginBottom: "32px" }}>
                                         <div className="p-2">
                                             <div className="campus-title-design">
                                                 <h5 className="card-title campus-fontfamily" style={{ fontSize: "24px" }}>{campus.Name}</h5>
-
                                             </div>
                                             <span className="">
                                                 <span className="fw-bold">Address: </span>{campus.Address}
@@ -79,11 +81,13 @@ function CoursesLocation({ courses }) {
                                                 </span>
                                             </div>
                                             <div className="mt-3">
-                                                <img
-                                                    src={require("../assets/instagram.png").default}
-                                                    alt=""
-                                                    className="instagram"
-                                                />
+                                                <a href={campus.Instagram_Id} target="_blank" rel="noopener noreferrer">
+                                                    <img
+                                                        src={require("../assets/instagram.png").default}
+                                                        alt="Instagram"
+                                                        className="instagram"
+                                                    />
+                                                </a>
                                             </div>
                                         </div>
                                     </div>
@@ -96,5 +100,5 @@ function CoursesLocation({ courses }) {
         </div>
     );
 }
-export default CoursesLocation;
 
+export default CoursesLocation;
--- src/Page/SchoolsPages/ScreningTest/index.js
+++ src/Page/SchoolsPages/ScreningTest/index.js
@@ -1,17 +1,24 @@
-import "./style.css"
+import React from "react";
+import "./style.css";
+
 function ScreeningTestPage() {
+    const handleButtonClick = () => {
+        window.open("https://admissions.navgurukul.org/", "_blank");
+    };
+
     return (
         <>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-12 col-sm-12 col-md-12" style={{ margin: "auto", marginBottom: "40px",marginTop:"40px" }}>
+                    <div className="col-lg-12 col-sm-12 col-md-12" style={{ margin: "auto", marginBottom: "40px", marginTop: "40px" }}>
                         <div className="next-step mt-2 mt-sm-3 d-flex flex-column align-items-center">
-                            <h5 className="text-cener" style={{marginBottom: "32px" }}>
-                            Want to Enroll  <span className="BackColor" >Want to Enroll and </span> Start your Career?
+                            <h5 className="text-center" style={{ marginBottom: "32px" }}>
+                                Want to Enroll <span className="BackColor">Want to Enroll and </span> Start your Career?
                             </h5>
                             <div className="input d-flex flex-column justify-content-center align-items-center">
                                 <button
-                                    className="btn btn-primary text-white align-self-center  my-1 regular-btn user-test-btn"
+                                    className="btn btn-primary text-white align-self-center my-1 regular-btn user-test-btn"
+                                    onClick={handleButtonClick}
                                 >
                                     Take Screening Test
                                 </button>
@@ -21,6 +28,7 @@ function ScreeningTestPage() {
                 </div>
             </div>
         </>
-    )
+    );
 }
+
 export default ScreeningTestPage;
