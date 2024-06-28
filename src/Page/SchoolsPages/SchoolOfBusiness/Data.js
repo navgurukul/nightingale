2d61382205636f9e5d8e55a0dee79786c2ab2c26
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-06-28 10:35:12 +0530

    img of file name change

--- src/Page/SchoolsPages/SchoolOfBusiness/Data.js
+++ src/Page/SchoolsPages/SchoolOfBusiness/Data.js
@@ -1,6 +1,6 @@
 import SchoolOfBusinessPage from "../SchoolOfBusiness/assets/businessbgimg.png"
-import KiranJatav from "./assets/Ellipse 158 (2).png";
-import MeenuGwala from "./assets/Ellipse 158 (1).png";
+import KiranJatav from "./assets/ KiranJatav.png";
+import MeenuGwala from "./assets/MeenuGwala.png";
 export const whoForPoints = [
    { point: "Youth above 16 years of age from underprivileged or low-income communities who can't afford expensive college education" },
    { point: "No prior education needed: Our students have been school and college dropouts, 10th or 12th pass as well as degree holders such as BSc, BTech who could not get jobs" },
@@ -8,23 +8,17 @@ export const whoForPoints = [
    { point: "Some level of English is good to have" },
    { point: "Have heard about programming or have an interest in making a career in this field" }
 ];
-
-
 export const topics = [
    { subtitle: 'Introduction to Google Suite:', text: "Learning advanced Google Sheets, slides, and forms" },
    { subtitle: "Introduction:", text: "Customer Relationship Management & Salesforce" },
    { subtitle: "Digital Marketing:", text: "Website building, Social media marketing & SEO" },
    { subtitle: "Data Analytics:", text: "Statistics, SQL queries, Tableau" }
 ]
-
-
 export const JobAssistanceData = [
    {
        JobAssistanceContent: "Students will get 100% assistance for securing jobs in roles such as Customer Relationship Representative, CRM Associate, Salesforce Associate & Operations Associate"
    }
 ]
-
-
 export const program3Data = [
    {
        buttonText: "Stage 1",
