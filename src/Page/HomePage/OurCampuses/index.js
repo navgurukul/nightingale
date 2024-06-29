24efd89ed985d616264ae952e21f4f5326cb2973
Apekshahande <apeksha18@navgurukul.org>
2024-06-29 21:46:28 +1100

    home pge bug fixing

--- src/Page/HomePage/OurCampuses/index.js
+++ src/Page/HomePage/OurCampuses/index.js
@@ -1,7 +1,7 @@
-import "./style.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import "./style.css";
 
 function OurCampuses() {
     const [data, setData] = useState(null);
@@ -12,11 +12,16 @@ function OurCampuses() {
             url: "https://navgurukul.github.io/tarabai-shinde/data/campuses.json",
         })
         .then((res) => {
+            console.log("Fetched data:", res.data);
             setData(
                 Object.keys(res.data).reduce((prev, next) => {
+                    console.log("Processing campus:", res.data[next].Name, "Image URL:", res.data[next].Img);
                     return { ...prev, [res.data[next].Name]: res.data[next] };
                 }, {})
             );
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
         });
     }, []);
 
@@ -36,7 +41,10 @@ function OurCampuses() {
                     {Object.keys(data).map((campusName) => {
                         const campus = data[campusName];
                         const displayName = campusName.split(" ")[0];
-                        if (campus.Name && campus.Img ) {
+
+                        if (campus.Name && campus.Img) {
+                            console.log(`Rendering ${displayName} with image ${campus.Img}`);
+                            
                             return (
                                 <div 
                                     className="col-sm-3 mb-3 mb-sm-0" 
@@ -44,16 +52,23 @@ function OurCampuses() {
                                     key={campusName} 
                                     onClick={() => { history.push(`/campus/${displayName}`) }}
                                 >
-                                    <div className="campus-title-design contant-box ">
-                                        <img className="campus-img mr-4" src={campus.Img} alt={`${displayName} image`} />
+                                    <div className="campus-title-design contant-box">
+                                        <img 
+                                            className="campus-img mr-4" 
+                                            src={campus.Img} 
+                                            alt={`${displayName} image`} 
+                                            onError={(e) => { e.target.src = 'https://via.placeholder.com/150?text=Image+Not+Available'; }} // Fallback image URL
+                                        />
                                         <div className="d-flex justify-content-center align-items-center mt-5">
-                                            <span className="mb-5 text-center font body1 ">
+                                            <span className="mb-5 text-center font body1">
                                                 {displayName}
                                             </span>
                                         </div>
                                     </div>
                                 </div>
                             );
+                        } else {
+                            console.error(`Missing data for ${displayName}:`, campus);
                         }
                     })}
                 </div>
