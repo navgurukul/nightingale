d42f50d3f2c2b035509f1cf740a7957e94e38fc7
Apekshahande <apeksha18@navgurukul.org>
2024-07-08 21:15:49 +1100

    refactor ourcampus page code which is in home page

--- src/Page/HomePage/OurCampuses/index.js
+++ src/Page/HomePage/OurCampuses/index.js
@@ -10,10 +10,8 @@ function OurCampuses() {
     useEffect(() => {
         axios.get("https://navgurukul.github.io/tarabai-shinde/data/campuses.json")
             .then((res) => {
-                console.log("Fetched data:", res.data);
                 setData(
                     Object.keys(res.data).reduce((prev, next) => {
-                        console.log("Processing campus:", res.data[next].Name, "Image URL:", res.data[next].Img);
                         return { ...prev, [res.data[next].Name]: res.data[next] };
                     }, {})
                 );
@@ -31,6 +29,10 @@ function OurCampuses() {
         );
     }
 
+    const handleCampusClick = (campusName) => {
+        history.push(`/campus/${campusName}`);
+    };
+
     return (
         <div className="container campus-main-page">
             <h3 className="text-center media-font">Our Campuses</h3>
@@ -40,17 +42,19 @@ function OurCampuses() {
                     const displayName = campusName.split(" ")[0];
 
                     if (campus.Name && campus.Img) {
-                        // Simplified image URL handling
                         const imageUrl = campus.Img.startsWith('http') ? campus.Img : `/images/${campus.Img}`;
-                        
-                        console.log(`Rendering ${displayName} with image ${imageUrl}`);
-                        console.log(`Image URL Validity Check: `, imageUrl);
-
                         return (
                             <div 
                                 className="col-sm-3 mb-4 mt-4 mb-sm-0" 
                                 key={campusName} 
-                                onClick={() => { history.push(`/campus/${displayName}`) }}
+                                role="button" 
+                                tabIndex="0"
+                                onClick={() => handleCampusClick(displayName)}
+                                onKeyDown={(e) => {
+                                    if (e.key === 'Enter' || e.key === ' ') {
+                                        handleCampusClick(displayName);
+                                    }
+                                }}
                             >
                                 <div className="campus-title-design contant-box">
                                     <img 
@@ -71,8 +75,8 @@ function OurCampuses() {
                             </div>
                         );
                     } else {
-                        console.error(`Missing data for ${displayName}:`, campus);
-                        return null; // Return null or handle missing data scenario
+                        console.warn(`Skipping ${campusName} due to incomplete data.`);
+                        return null;
                     }
                 })}
             </div>
