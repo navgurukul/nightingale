b3e6ff5d5580a4a0c320b4e35d564f1aeaf8a3b9
Apekshahande <apeksha18@navgurukul.org>
2024-08-02 23:37:01 +1100

    new page

--- src/Page/NgHiring/index.js
+++ src/Page/NgHiring/index.js
@@ -1,73 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
+import OurAlumni from "./Ouralumni";
+import Timeline from "./Timeline";
+import Slider from "./Slider";
+import TwoSectionLayout from "./TwoSectionlayout";
 
 function NgHiring() {
   return (
     <main >
-      <section className="container align-items-center d-flex flex-column  justify-content-center w-100">
-        <h3 className="section-head mt-3 mt-md-5 mb-4">Hire from Us</h3>
-        <div className=" hiring-page-content col-12 p-0 col-md-8 section-para d-flex align-items-center justify-content-center ">
-          <p className="section-para body1  w-100" >
-            Through harnessing the power of education, We bridge the learning 
-            gap of students from marginalized communities to emerge as skilled,
-            resilient, and job-ready professionals, proficient in the latest technologies
-            and soft skills. By partnering with us, your organization can enrich its 
-            workforce diversity and drive meaningful social impact, effortlessly meeting
-            your Diversity, Equity, and Inclusion (DEI) goals.
-          </p>
-        </div>
-        <div className=" hiring-page-content col-12 p-0 col-md-8 section-para d-flex align-items-center justify-content-center  mt-2">
-          <p className="section-para body1  w-100" >
-            By hiring from us, You are achieving : 
-            <ul className="ml-4">
-              <li>
-                Handpicked Tech Talent: Pre-screened candidates to save your time.
-              </li>
-              <li>
-                Zero Hiring Fees: Fully funded by our generous donors.
-              </li>
-              <li>
-                High Retention candidates: Quality talent within budget</li>
-              <li>
-                Diverse Talent Pool: Our students are from different regions 
-                of India. Ready to enhance your workforce diversity and business capabilities.
-              </li>
-              <li>
-                Job-Ready Talent: Our students undergo rigorous training in both 
-                technical and soft skills, preparing them for the complexities of modern
-                work environments.
-              </li>
-              <li>
-                Positive Social Impact: Fulfill your corporate social responsibility goals by hiring candidates who are part of a movement to bridge the education and employment gap in India.
-              </li>
-            </ul>
-          </p>
-        </div>
-         <div className=" hiring-page-content col-12 p-0 col-md-8 section-para d-flex align-items-center justify-content-center  mt-2 mb-4 ">
-          <p className="section-para body1  w-100" >
-              Contact us directly at{" "}
-              <a href="mailto:tnp@navgurukul.org" className="link">
-                tnp@navgurukul.org
-              </a>{" "}
-               for hiring or freelancing projects.
-          </p>
-        </div>
-      
-         <a
-            href="https://drive.google.com/file/d/1sem3BGOxBscPY2KMOcc_KAZ1ZJSJ-IeE/view?usp=sharing"
-            download="Placement Brief - Navgurukul"
-            target="_blank"
-          >
-            <button
-              type="button"
-              class="btn mb-5  f-Nuni fw-bold py-2 regular-btn align-self-center px-4 py-2"
-              style={{width : "360px"}}
-            >
-              Download Placement Brief
-            </button>
-          </a>
-         
-        </section>    
+      <Timeline /> 
+      <OurAlumni />  
+      <Slider />
+      <TwoSectionLayout />
     </main>
   );
 }
