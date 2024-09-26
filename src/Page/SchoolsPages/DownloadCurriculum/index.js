ee1db4b64bcf590962328740255cc58a6296079a
Your Name <your.email@example.com>
2024-09-26 15:27:17 +0530

    work on pdf file

--- src/Page/SchoolsPages/DownloadCurriculum/index.js
+++ src/Page/SchoolsPages/DownloadCurriculum/index.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import "./style.css";
-import { saveAs } from 'file-saver';
+import { Link } from 'react-router-dom';
 
 function DownloadCurriculum({ DownloadData }) {
     const onButtonClick = (pdfFile) => {
-        
-        const pdfUrl = pdfFile;
+        console.log(pdfFile);
+        // Create an anchor element
         const link = document.createElement("a");
-        link.href = pdfUrl;
-        link.download = "Curriculam.pdf"; // specify the filename
+        link.href = pdfFile; // Directly set the PDF file URL
+        link.target = "_blank"; // Optional: open in a new tab
+        link.download = "Curriculum.pdf"; // Specify the default filename for download
+        console.log(link);
+        // Append the link to the body
         document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        link.click(); // Programmatically click the link to trigger the download
+        document.body.removeChild(link); // Clean up the DOM
+        
     };
-    
-    
 
     return (
-        <div className="container d-flex justify-content-sm-center DownloadCurriculumContainer ">
-            
+        <div className="container d-flex justify-content-sm-center DownloadCurriculumContainer">
             {DownloadData && DownloadData.map((data) => (
-                <button
-                    key={data.id}
-                    type="button"
-                    className="btn regular-btn"
-                    style={{ height: "48px", width: "208px", margin: "10px" }}
-                    onClick={() => onButtonClick(data.pdfFile)}
-                >
-                    Download Curriculum 
-                </button>
+                <a href={data.pdfFile} target="_blank" rel="noopener noreferrer">
+                    <button
+                        key={data.id}
+                        type="button"
+                        className="btn regular-btn"
+                        style={{ height: "48px", width: "208px", margin: "10px" }}
+                        
+                    >
+                        Download Curriculum 
+                    </button>
+                </a>
             ))}
         </div>
     );
 }
 
 export default DownloadCurriculum;
-
