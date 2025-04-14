5e2d4f8a45805a8833ba27913b9c20a8cd6bb154
NIKITAVERM123 <nikitaverma21@navgurukul.org>
2025-04-14 12:06:40 +0530

    Fix: Resolved all CSS issues based on Figma design

--- src/Page/HomePage/AlumniStories/data.js
+++ src/Page/HomePage/AlumniStories/data.js
@@ -1,17 +1,18 @@
 export const data = [
     {
-        id: 1,
-        video: "https://www.youtube.com/embed/DELF_PyG_E4",
-        name: "Annu Bharti’s Story",
-    }, 
+      id: 1,
+      video: "https://www.youtube.com/embed/DELF_PyG_E4",
+      name: "Annu Bharti’s Story",
+    },
     {
-        id: 2,
-        video: "https://www.youtube.com/embed/XJ6hcfkSDkw",
-        name: "Rani Pandit’s Story"
-    }, 
+      id: 2,
+      video: "https://www.youtube.com/embed/XJ6hcfkSDkw",
+      name: "Rani Pandit’s Story",
+    },
     {
-        id: 3,
-        video:"https://www.youtube.com/embed/o3mXHf7LWiw",
-        name: "Kajal Agarwal’s Story"
-    }
-];
+      id: 3,
+      video: "https://www.youtube.com/embed/o3mXHf7LWiw",
+      name: "Kajal Agarwal’s Story",
+    },
+  ];
+  
--- src/Page/TeamPage/index.js
+++ src/Page/TeamPage/index.js
@@ -40,6 +40,18 @@ function TeamPage() {
 
   return (
     <main className="team-page">
+      <div className="text-center mx-auto p-4" style={{ maxWidth: "736px" }}>
+        <h3 className="mb-4">Our Team</h3>
+        <p className="mb-2" style={{ textAlign: "left", fontSize: "18px" }}>
+          We are a collective of full-timers and volunteers who form the
+          backbone of a movement aiming to bring affordable education to
+          underprivileged girl students across India. Our workforce composition
+          is consciously diverse and inclusive, with 65% of our team members
+          being NavGurukul Alumni, and over 67% of team members being women and
+          those who identify in the gender spectrum as non-binary.
+        </p>
+      </div>
+
       <div className="team-content">
         <section className="team-section w-100 mt-4 d-flex flex-column justify-center align-items-center">
           <div className="container px-0 team-info-cards-container mt-4">
@@ -49,30 +61,36 @@ function TeamPage() {
                   <h5 className="team-heading">Team</h5>
                   <ul className="nav nav-tabs flex-column">
                     {[
-                      "Core Team", 
-                      "Ghar", 
-                      "Residential Programme", 
-                      "People and Culture", 
-                      "Placements", 
-                      "Operations", 
-                      "Academics", 
-                      "Admissions", 
+                      "Core Team",
+                      "Ghar",
+                      "Residential Programme",
+                      "People and Culture",
+                      "Placements",
+                      "Operations",
+                      "Academics",
+                      "Admissions",
                       "LXD & ETC",
-                      "Culture"
                     ].map((teamName) => (
                       <li
                         key={teamName}
-                        className={`nav-item team-list-item ${selectedTeam === teamName ? "active" : ""}`}
+                        className={`nav-item team-list-item ${
+                          selectedTeam === teamName ? "active" : ""
+                        }`}
                         onClick={() => setSelectedTeam(teamName)}
                       >
-                        <span className="nav-link text-left mt-2">{teamName}</span>
+                        <span className="nav-link text-left mt-2">
+                          {teamName}
+                        </span>
                       </li>
                     ))}
                   </ul>
                 </div>
               </div>
               <div className="container-fluid w-100 mt-4">
-                <h5 className="team-heading mb-2" style={{ position: "relative", bottom: "30px" }}>
+                <h5
+                  className="team-heading mb-2"
+                  style={{ position: "relative", bottom: "30px" }}
+                >
                   {selectedTeam}
                 </h5>
 
@@ -92,8 +110,12 @@ function TeamPage() {
                               alt={member.Name}
                             />
                           </div>
-                          <p className="team-info-card-title body">{member.Name}</p>
-                          <p className="team-info-card-designation">{member["Teaching Fellow"]}</p>
+                          <p className="team-info-card-title body">
+                            {member.Name}
+                          </p>
+                          <p className="team-info-card-designation">
+                            {member["Teaching Fellow"]}
+                          </p>
                         </div>
                       </div>
                     ))
